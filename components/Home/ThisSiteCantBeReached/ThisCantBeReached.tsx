499141d31ce7a629fdbe55979c475d536f8b2f69
Saidul-M-Khan <19-41766-3@student.aiub.edu>
2023-12-08 05:06:37 +0600

    done

--- components/Home/ThisSiteCantBeReached/ThisCantBeReached.tsx
+++ components/Home/ThisSiteCantBeReached/ThisCantBeReached.tsx
@@ -191,7 +191,7 @@ export default function ThisCantBeReached() {
             be reached
           </span>
           <span className="text-gray-500 text-md">
-            <span className="font-bold">www.khansaab.com </span>
+            <span className="font-bold">https://portfolio-saidul-mursalin-khans-projects.vercel.app </span>
             unexpectedly{" "}
             {ShowText ? (
               <motion.span
